32f5b99ea81a691c08ae203e800cd155ab028710
Anne Saber <iarabejo3@gmail.com>
2022-05-10 12:07:30 -0500

    Imported all components to index.js file and declared routes.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -28,7 +28,7 @@ function Navigation() {
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/logout">
+                <NavLink className="nav-link" to="/">
                   Logout
                 </NavLink>
               </li>
