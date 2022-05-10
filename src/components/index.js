cfcb2cb32370244a48d1bb306530f14dcb588588
Anne Saber <iarabejo3@gmail.com>
2022-05-10 11:57:05 -0500

    Added index.js file inside components directory.

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Navigation } from "./Navigation";
+export { default as Footer } from "./Footer";
+export { default as Home } from "./";
+export { default as Dashboard } from "./dashboard";
+export { default as Resume } from "./resume-builder";
+export { default as Logout } from "./";
