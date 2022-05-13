6fff836fd4781df816ceba40af690e107e64f475
Matt4292 <matt4292002@gmail.com>
2022-05-13 11:08:32 -0500

    login routes and some logic

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,24 +3,58 @@ const User = require('../models/User')
 exports.register = async (req, res, next) => {
   const {username, email, password} = req.body;
 
+  const usernameUsed = await User.findOne({username})
+  if (usernameUsed) {
+    return (res.send("Username already taken, try being creative for once"))    
+  }
+
+  const emailUsed = await User.findOne({email})
+  if (emailUsed) {
+    return (res.send("Email already in use"))    
+  }
+
   try {
     const user = await User.create({
       username, email, password
     });
-
     res.status(201).json({
       success: true,
+      user
     })
-  } catch (error) {
+  } catch (err) {
+    console.log(err)
     res.status(500).json({
       success: false,
-      error: error.message
+      error: err.message
     })
   }
-
-  res.send("Register Route");
 };
 
-exports.login = (req, res, next) => {
-  res.send("Login Route");
+exports.login = async (req, res, next) => {
+  const { email, password} = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({success: false, error: "Enter your email and password"})
+  }
+
+  try {
+    const user = await User.findOne({email}).select("+password");
+
+    if (!user) {
+      return (res.status(404).json({success: false, error: "Nah bro try again"}))
+    }
+
+    const isMatch = await user.matchPasswords(password);
+
+    if(!isMatch) {
+      return (res.status(404).json({success: false, error: "nah bro try again"}))
+    }
+
+    return res.status(200).json({
+      success: true,
+      token: "fh82fhdgj3"
+    });
+  } catch (err) {
+    console.log(err)
+  }
 };
--- server/models/User.js
+++ server/models/User.js
@@ -4,13 +4,14 @@ const bcrypt = require('bcrypt');
 const userSchema = new Schema({
   username: {
     type: String,
+    unique: true,
     required: true,
   },
   email: {
     type: String,
     required: true,
     unique: true,
-
+    isEmail: true
   },
   password: {
     type: String,
@@ -18,14 +19,19 @@ const userSchema = new Schema({
   },
 
 });
-userSchema.pre('save', async function() {
-  if (!this.isModified("password")) {
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) {
     next()
   } 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt)
   next();
 });
+
+userSchema.methods.matchPasswords = async function( password ) {
+  return await bcrypt.compare(password, this.password);
+}
 
 const User = model('user', userSchema);
 module.exports = User;
