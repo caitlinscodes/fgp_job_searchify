4ac1a945f954f428a68ff460f4d4c38848e765e5
Anne Saber <iarabejo3@gmail.com>
2022-05-13 15:53:07 -0500

    Listed out user info including experience and stylized pdf formatting.

--- client/src/components/PDF.jsx
+++ client/src/components/PDF.jsx
@@ -6,22 +6,27 @@ const ref = React.createRef();
 const PDF = (props) => {
   return (
     <>
-      <div className="Post" ref={ref}>
+      <div className="Resume" class="PDF-format" ref={ref}>
         <h1>{props.name}</h1>
         <p>{props.email}</p>
         <p>{props.phone}</p>
         <p>{props.address}</p>
-        <p>{props.city}</p>
-        {/* <p>{props.state}</p>
-        <p>{props.zip}</p>
-        <textarea>{props.summary}</textarea>
-        <p>{props.title}</p> */}
-        {/* <p>{props.company}</p>
-        <textarea>{props.experience}</textarea>
-        <textarea>{props.skills}</textarea> */}
+        <p>{props.location}</p>
+        <hr/>
+        <p>{props.summary}</p>
+        <hr/>
+        <p>{props.skills}</p>
+        <hr/>
+        <p>{props.title1}</p>
+        <p>{props.company1}</p>
+        <p>{props.experience1}</p>
+        <hr/>
+        <p>{props.title2}</p>
+        <p>{props.company2}</p>
+        <p>{props.experience2}</p>
       </div>
       <Pdf targetRef={ref} filename="post.pdf">
-        {({ toPdf }) => <button onClick={toPdf}>Download as Resume</button>}
+        {({ toPdf }) => <button id="pdf-btn" className="btn btn-primary btn-m" onClick={toPdf}>Download Resume</button>}
       </Pdf>
     </>
   );
--- client/src/components/Resume.jsx
+++ client/src/components/Resume.jsx
@@ -7,15 +7,16 @@ class Resume extends Component {
       email: '',
       phone: '',
       address: '',
-      city: '',
-      // state: '',
-      // zip: '',
-      // summary: ``,
-      // title: '',
-      // company: '',
-      // experience: '',
-      // skills: ``,
-      postSubmitted: false
+      location: '',
+      summary: '',
+      skills: ``,
+      title1: '',
+      company1: '',
+      experience1: '',
+      title2: '',
+      company2: '',
+      experience2: '',
+      resumeSubmitted: false
   }
 
   onChange = input => e => {
@@ -24,22 +25,22 @@ class Resume extends Component {
       });
   }
 
-  submitPost = (e) => {
+  submitResume = (e) => {
       
-      if(!this.state.name || !this.state.email || !this.state.phone || !this.state.address || !this.state.city ){
+      if(!this.state.name || !this.state.email || !this.state.phone || !this.state.address || !this.state.location || !this.state.summary || !this.state.skills || !this.state.title1 || !this.state.company1 || !this.state.experience1 || !this.state.title2 || !this.state.company2 || !this.state.experience2 ){
           alert('All fields are required!');
           e.preventDefault();
       }else{
           this.setState({
-              postSubmitted: true
+              resumeSubmitted: true
           });
       }
   }
   render(){
     return(
         <>
-            {  !this.state.postSubmitted ? 
-                (<div className="container">
+            {  !this.state.resumeSubmitted ? 
+                (<div className="container" class="resume-builder">
                     <div className="jumbotron mt-3">
                         <div className="row">
                             <div className="col-md-12">
@@ -51,25 +52,58 @@ class Resume extends Component {
                                                 <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-user bigicon"></i></span>
                                                 <input onChange={this.onChange('name')} name="name" type="text" placeholder="Full Name" className="form-control" />
                                             </div>
-                                            
                                             <div className="form-group">
                                                 <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-pencil-square-o bigicon"></i></span>
-                                                <input onChange={this.onChange('email')} className="form-control" name="email" placeholder="Email" ></input>
+                                                <input onChange={this.onChange('email')} className="form-control" name="email" placeholder="Email" />
                                             </div>
                                             <div className="form-group">
                                                 <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-pencil-square-o bigicon"></i></span>
-                                                <input onChange={this.onChange('phone')} className="form-control" name="phone" placeholder="Phone Number" rows="7"></input>
+                                                <input onChange={this.onChange('phone')} className="form-control" name="phone" placeholder="Phone Number" rows="7"/>
                                             </div>
                                             <div className="form-group">
                                                 <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-pencil-square-o bigicon"></i></span>
-                                                <input onChange={this.onChange('address')} className="form-control" name="address" placeholder="Address" ></input>
+                                                <input onChange={this.onChange('address')} className="form-control" name="address" placeholder="Address" />
                                             </div>
                                             <div className="form-group">
                                                 <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-pencil-square-o bigicon"></i></span>
-                                                <input onChange={this.onChange('city')} className="form-control" name="city" placeholder="City" ></input>
+                                                <input onChange={this.onChange('location')} className="form-control" type="text" name="location" placeholder="City, State, Zip" />
+                                            </div>
+                                            <hr/>
+                                            <div className="form-group">
+                                                <span className="text-center"><i className="fa fa-user bigicon"></i></span>
+                                                <input onChange={this.onChange('summary')} className="form-control" type="text" name="summary" placeholder="Summary" rows={5}/>
+                                            </div>
+                                            <hr/>
+                                            <div className="form-group">
+                                                <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-pencil-square-o bigicon"></i></span>
+                                                <input onChange={this.onChange('skills')} className="form-control" type="text" name="skills" placeholder="Soft and Hard Skills" rows={3}/>
                                             </div>
                                             <div className="form-group">
-                                                <button type="button" onClick={this.submitPost} className="btn btn-primary btn-lg">Submit</button>
+                                                <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-pencil-square-o bigicon"></i></span>
+                                                <input onChange={this.onChange('title1')} className="form-control" type="text" name="title1" placeholder="Title"/>
+                                            </div>
+                                            <div className="form-group">
+                                                <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-pencil-square-o bigicon"></i></span>
+                                                <input onChange={this.onChange('company1')} className="form-control" type="text" name="company1" placeholder="Company"/>
+                                            </div>
+                                            <div className="form-group">
+                                                <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-pencil-square-o bigicon"></i></span>
+                                                <input onChange={this.onChange('experience1')} className="form-control" type="text" name="experience1" placeholder="Experience" rows={5}></input>
+                                            </div>
+                                            <div className="form-group">
+                                                <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-pencil-square-o bigicon"></i></span>
+                                                <input onChange={this.onChange('title2')} className="form-control" type="text" name="title2" placeholder="Title"/>
+                                            </div>
+                                            <div className="form-group">
+                                                <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-pencil-square-o bigicon"></i></span>
+                                                <input onChange={this.onChange('company2')} className="form-control" type="text" name="company2" placeholder="Company"/>
+                                            </div>
+                                            <div className="form-group">
+                                                <span className="col-md-1 col-md-offset-2 text-center"><i className="fa fa-pencil-square-o bigicon"></i></span>
+                                                <input onChange={this.onChange('experience2')} className="form-control" type="text" name="experience2" placeholder="Experience" rows={5}/>
+                                            </div>
+                                            <div className="form-group">
+                                                <button type="button" onClick={this.submitResume} className="btn btn-primary btn-lg">Submit</button>
                                             </div>
                                         </fieldset>
                                     </form>
@@ -78,7 +112,7 @@ class Resume extends Component {
                         </div>
                     </div>
                 </div>) : (
-                    <PDF name={this.state.name} email={this.state.email} phone={this.state.phone} address={this.state.address} city={this.state.city} />
+                    <PDF name={this.state.name} email={this.state.email} phone={this.state.phone} address={this.state.address} location={this.state.location} summary={this.state.summary} skills={this.state.skills} title1={this.state.title1} company1={this.state.company1} experience1={this.state.experience1} title2={this.state.title2} company2={this.state.company2} experience2={this.state.experience2}/>
                 )
             }
         </>
