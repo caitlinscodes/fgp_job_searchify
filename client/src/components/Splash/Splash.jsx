d79e2eecf845a1483603c1cac73b9490a38db2a6
Jonathan Johnson <jonathandila682@gmail.com>
2022-05-16 12:47:13 -0500

    Add Contact info for Splash page

--- client/src/components/Splash/Splash.jsx
+++ client/src/components/Splash/Splash.jsx
@@ -15,60 +15,92 @@ import Resume from "../../assets/resume_img.png";
 function Splash() {
   return (
     <ThemeProvider>
-    <section className="splash-container">
-      <Container className="splash-page" id="splash">
-        <Container className="splash-main">
-          <Row>
-            {/* <h1>JOB.searchify(</h1><Type /><h1>)</h1> */}
-            <img src={Logo} alt="JOB.searchify logo" className="img-fluid" />
-            
-          </Row>
-        </Container>
-        <br></br>
-        <Container className="splash-description">
-          <Row>
-            <Col><img src={JobSearch} alt="computer"/></Col>
-            <Col>
-              <h3>Find Your Next Tech Job</h3>
-              <p>Looking for a career in tech but don't know where to start? Our tech industry job search platform is your one stop shop for your next job search. Our user friendly search engine weeds through thousands of postings to bring you the best tech jobs around. Happy Job Hunting!</p>
-            </Col>
-          </Row>
+      <section className="splash-container">
+        <Container className="splash-page" id="splash">
+          <Container className="splash-main">
+            <Row>
+              {/* <h1>JOB.searchify(</h1><Type /><h1>)</h1> */}
+              <img src={Logo} alt="JOB.searchify logo" className="img-fluid" />
+            </Row>
+          </Container>
           <br></br>
-          <Row>
-            <Col>
-              <h3>Build the Perfect Resume</h3>
-              <p>Crafting a resume can be time consuming and confusing, but with our built in Resume Builder you'll have a professional CV ready in minutes. Put your best foot forward in the job application process and create an account with us to start building your resume today!</p>
-            </Col>
-            <Col><img src={Resume} alt="resume"/></Col>
-          </Row>
-        </Container>
-        <br></br>
-        <Container className="splash-about">
-          <h3>About the Creators</h3>
-          <Row>
-            <Col>
-              <img src={IArabejo} alt="Inna Profile"/>
-              <h5>Inna Arabejo</h5>
-              <p>My name is Inna Arabejo, and I am an aspiring full stack developer versed in building projects from the ground up with a primary focus on MERN Stack web applications. </p>
-            </Col>
-            <Col>
-              <img src={MJackson} alt="Matt Profile"/>
-              <h5>Matt Jackson</h5>
-              <p>Web Developer Currently enrolled at SMU Boot Camp </p>
-            </Col>
-            <Col>
-              <img src={JJohnson} alt="Jonathan Johnson"/>
-              <h5>Johnathan Johnson</h5>
-              <p>Passionate about writing clean, readable, and reusable code? Interested in building frontend and backend web applications, I also dibble and dabble in robotics. </p>
-            </Col>
-            <Col>
-              <img src={CStev} alt="Caitlin Profile"/>
-              <h5>Caitlin Stevenson</h5>
-              <p>Howdy! I am an experienced professional transitioning into the web development field. </p>
-            </Col>
-          </Row>
-        </Container>
-        <br></br>
+          <Container className="splash-description">
+            <Row>
+              <Col>
+                <img src={JobSearch} alt="computer" />
+              </Col>
+              <Col>
+                <h3>Find Your Next Tech Job</h3>
+                <p>
+                  Looking for a career in tech but don't know where to start?
+                  Our tech industry job search platform is your one stop shop
+                  for your next job search. Our user friendly search engine
+                  weeds through thousands of postings to bring you the best tech
+                  jobs around. Happy Job Hunting!
+                </p>
+              </Col>
+            </Row>
+            <br></br>
+            <Row>
+              <Col>
+                <h3>Build the Perfect Resume</h3>
+                <p>
+                  Crafting a resume can be time consuming and confusing, but
+                  with our built in Resume Builder you'll have a professional CV
+                  ready in minutes. Put your best foot forward in the job
+                  application process and create an account with us to start
+                  building your resume today!
+                </p>
+              </Col>
+              <Col>
+                <img src={Resume} alt="resume" />
+              </Col>
+            </Row>
+          </Container>
+          <br></br>
+          <Container className="splash-about">
+            <h3>About the Creators</h3>
+            <Row>
+              <Col>
+                <img src={IArabejo} alt="Inna Profile" />
+                <h5>Inna Arabejo</h5>
+                <p>
+                  My name is Inna Arabejo, and I am an aspiring full stack
+                  developer versed in building projects from the ground up with
+                  a primary focus on MERN Stack web applications.{" ~ "}
+                  <a href="https://github.com/inna-arabejo">Contact Inna</a>
+                </p>
+              </Col>
+              <Col>
+                <img src={MJackson} alt="Matt Profile" />
+                <h5>Matt Jackson</h5>
+                <p>
+                  Web Developer Currently enrolled at SMU Boot Camp{" ~ "}
+                  <a href="https://github.com/Matt4292">Contact Matt</a>
+                </p>
+              </Col>
+              <Col>
+                <img src={JJohnson} alt="Jonathan Johnson" />
+                <h5>Johnathan Johnson</h5>
+                <p>
+                  Passionate about writing clean, readable, and reusable code?
+                  Interested in building frontend and backend web applications,
+                  I also dibble and dabble in robotics.{" ~ "}
+                  <a href="https://github.com/jayBassMan">Contact Jonathan</a>
+                </p>
+              </Col>
+              <Col>
+                <img src={CStev} alt="Caitlin Profile" />
+                <h5>Caitlin Stevenson</h5>
+                <p>
+                  Howdy! I am an experienced professional transitioning into the
+                  web development field.{" ~ "}
+                  <a href="https://github.com/caitlinscodes">Contact Caitlin</a>
+                </p>
+              </Col>
+            </Row>
+          </Container>
+          <br></br>
           {/* Router to call in Contact Form
           <Router>
             <Routes>
@@ -76,10 +108,9 @@ function Splash() {
             </Routes>
           </Router> */}
 
-        <ContactForm />
-
-      </Container>
-    </section>
+          <ContactForm />
+        </Container>
+      </section>
     </ThemeProvider>
   );
 }
